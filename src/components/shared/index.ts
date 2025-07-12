661a5d79ed3aa43fb6ce1dc5ed5015f122521c2f
noahspott <noahspottdev@gmail.com>
2025-07-12 13:40:32 -0300

    feat: SubpageContent Schedule Page

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -3,3 +3,4 @@ export { default as Socials } from "./Socials";
 export { default as Section } from "./Section";
 export { default as Link } from "./Link";
 export { default as Button } from "./Button";
+export { default as SubpageContent } from "./SubpageContent.astro";
