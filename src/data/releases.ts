75921a0583a72429c88d6060f9a83a4ebb7ebb5d
noahspott <noahspottdev@gmail.com>
2025-07-12 18:28:23 -0300

    feat: music page

---
+++ src/data/releases.ts
@@ -0,0 +1,112 @@
+import type { ReleaseType } from "@/types/ReleaseType";
+
+export const releases: ReleaseType[] = [
+  {
+    name: "Make You Mine",
+    image: {
+      src: "/images/releases/make-you-mine.jpg",
+      alt: "Cover art for Make You Mine",
+    },
+    releaseDate: new Date("2022-06-10"),
+    listenLink: "https://open.spotify.com/album/1abc2def3ghi4jkl5mno6pq",
+  },
+  {
+    name: "World Go 'Round",
+    image: {
+      src: "/images/releases/world-go-round.jpg",
+      alt: "Cover art for World Go 'Round",
+    },
+    releaseDate: new Date("2024-07-05"),
+    listenLink: "https://open.spotify.com/track/7xyz8abc9def0ghi1jkl2mn",
+  },
+  {
+    name: "Kiss Me",
+    image: {
+      src: "/images/releases/kiss-me.jpg",
+      alt: "Cover art for Kiss Me",
+    },
+    releaseDate: new Date("2023-02-14"),
+    listenLink: "https://open.spotify.com/track/3mno4pqr5stu6vwx7yz8ab",
+  },
+  {
+    name: "Wild Things",
+    image: {
+      src: "/images/releases/wild-things.jpg",
+      alt: "Cover art for Wild Things",
+    },
+    releaseDate: new Date("2018-09-28"),
+    listenLink: "https://open.spotify.com/album/4ghi5jkl6mno7pqr8stu9vw",
+  },
+  {
+    name: "Heartland Highway",
+    image: {
+      src: "/images/releases/heartland-highway.jpg",
+      alt: "Cover art for Heartland Highway",
+    },
+    releaseDate: new Date("2021-11-19"),
+    listenLink: "https://open.spotify.com/album/9jkl0mno1pqr2stu3vwx4yz",
+  },
+  {
+    name: "Cold Nights & Campfires",
+    image: {
+      src: "/images/releases/cold-nights.jpg",
+      alt: "Cover art for Cold Nights & Campfires",
+    },
+    releaseDate: new Date("2023-12-01"),
+    listenLink: "https://open.spotify.com/track/1pqr2stu3vwx4yz5abc6de",
+  },
+  {
+    name: "Backroad Believer",
+    image: {
+      src: "/images/releases/backroad-believer.jpg",
+      alt: "Cover art for Backroad Believer",
+    },
+    releaseDate: new Date("2020-08-08"),
+    listenLink: "https://open.spotify.com/album/6mno7pqr8stu9vw0xyz1ab",
+  },
+  {
+    name: "Small Town Summer",
+    image: {
+      src: "/images/releases/small-town-summer.jpg",
+      alt: "Cover art for Small Town Summer",
+    },
+    releaseDate: new Date("2019-07-04"),
+    listenLink: "https://open.spotify.com/track/2abc3def4ghi5jkl6mno7pq",
+  },
+  {
+    name: "Whiskey Sunsets",
+    image: {
+      src: "/images/releases/whiskey-sunsets.jpg",
+      alt: "Cover art for Whiskey Sunsets",
+    },
+    releaseDate: new Date("2021-03-26"),
+    listenLink: "https://open.spotify.com/track/8stu9vw0xyz1abc2def3gh",
+  },
+  {
+    name: "The Way You Lie",
+    image: {
+      src: "/images/releases/the-way-you-lie.jpg",
+      alt: "Cover art for The Way You Lie",
+    },
+    releaseDate: new Date("2022-01-14"),
+    listenLink: "https://open.spotify.com/track/0xyz1abc2def3ghi4jkl5mn",
+  },
+  {
+    name: "Raised on the Radio",
+    image: {
+      src: "/images/releases/raised-on-the-radio.jpg",
+      alt: "Cover art for Raised on the Radio",
+    },
+    releaseDate: new Date("2020-05-15"),
+    listenLink: "https://open.spotify.com/album/3def4ghi5jkl6mno7pqr8st",
+  },
+  {
+    name: "Long Drive Home",
+    image: {
+      src: "/images/releases/long-drive-home.jpg",
+      alt: "Cover art for Long Drive Home",
+    },
+    releaseDate: new Date("2019-10-10"),
+    listenLink: "https://open.spotify.com/album/5jkl6mno7pqr8stu9vw0xy",
+  },
+];
