9db687682000f549b6cb086e211491601b4e3efc
noahspott <noahspottdev@gmail.com>
2025-07-22 08:59:11 -0300

    styles: make hero chevrons thinner and sized better

--- src/components/decorative/ScrollArrows.tsx
+++ src/components/decorative/ScrollArrows.tsx
@@ -1,11 +1,22 @@
-import { FaChevronDown } from "react-icons/fa6";
+import { LuChevronDown } from "react-icons/lu";
 
 export default function ScrollArrows({ className }: { className: string }) {
+  const iconClass = "-mt-16 size-22";
+  const strokeWidth = 1;
   return (
     <div className={`flex flex-col items-center ${className}`}>
-      <FaChevronDown className="arrow1 -mt-6 size-12" />
-      <FaChevronDown className="arrow2 -mt-6 size-12" />
-      <FaChevronDown className="arrow3 -mt-6 size-12" />
+      <LuChevronDown
+        className={`arrow1 ${iconClass}`}
+        strokeWidth={strokeWidth}
+      />
+      <LuChevronDown
+        className={`arrow2 ${iconClass}`}
+        strokeWidth={strokeWidth}
+      />
+      <LuChevronDown
+        className={`arrow3 ${iconClass}`}
+        strokeWidth={strokeWidth}
+      />
     </div>
   );
 }
