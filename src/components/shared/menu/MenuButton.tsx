837628de445a21bbe9d78a9b9409513d1c417ff8
noahspott <noahspottdev@gmail.com>
2025-07-22 08:20:58 -0300

    feat: menu animation

--- src/components/shared/menu/MenuButton.tsx
+++ src/components/shared/menu/MenuButton.tsx
@@ -1,17 +1,37 @@
 // Lib
 import { useState } from "react";
 import { useScrollLock } from "./useScrollLock";
+import { motion, AnimatePresence } from "framer-motion";
 
 // Components
 import MenuModal from "./MenuModal";
 import { FaBars } from "react-icons/fa6";
 
-export default function MenuButton() {
+export default function MenuButton({ className = "" }: { className?: string }) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   useScrollLock(isOpen);
+
   return (
-    <button onClick={() => setIsOpen((prev) => !prev)}>
-      {isOpen ? <MenuModal /> : <FaBars className="menu-button size-6" />}
-    </button>
+    <AnimatePresence mode="wait">
+      {isOpen ? (
+        <MenuModal setIsOpen={setIsOpen} />
+      ) : (
+        <motion.div
+          initial={{ x: "40px", opacity: 0 }}
+          animate={{ x: "0", opacity: 1 }}
+          key="menu-button"
+        >
+          <motion.button
+            className={`hover:cursor-pointer ${className}`}
+            onClick={() => {
+              setIsOpen((prev) => !prev);
+            }}
+            aria-label="Open menu"
+          >
+            <FaBars className="text-secondary size-6" />
+          </motion.button>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
--- src/components/shared/menu/MenuModal.tsx
+++ src/components/shared/menu/MenuModal.tsx
@@ -1,20 +1,38 @@
 // Components
 import { FaXmark } from "react-icons/fa6";
+import { motion } from "motion/react";
 
 // Data
 import { links } from "@/data/links";
 
-export default function MenuModal() {
+export default function MenuModal({
+  setIsOpen,
+}: {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   return (
-    <div className="bg-primary absolute inset-0 z-50 h-screen w-screen">
-      <FaXmark className="text-secondary menu-button absolute top-11 right-4 size-6" />
+    <motion.section
+      key="menu-modal"
+      initial={{ x: "100%", opacity: 1 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: "100%", opacity: 0 }}
+      transition={{ type: "tween", duration: 0.1, stiffness: 50, damping: 10 }}
+      className="bg-primary/70 fixed inset-0 z-50 flex h-screen w-screen items-center justify-center backdrop-blur-lg"
+    >
       <div className="relative h-full w-full">
-        <nav className="me-8 mt-30 flex flex-col gap-8">
+        <button className="absolute top-9 right-4">
+          <FaXmark
+            className="text-lunar-white size-6 hover:cursor-pointer"
+            onClick={() => setIsOpen(false)}
+            aria-label="Close menu"
+          />
+        </button>
+        <nav className="me-8 mt-48 flex flex-col items-center gap-8">
           {links.map((link) => {
             return (
               <li className="list-none">
                 <a
-                  className="font-heading font-2 text-lunar-white hover:text-lunar-accent text-right text-4xl uppercase duration-150 hover:cursor-pointer"
+                  className="font-heading font-2 text-secondary hover:text-lunar-accent text-right text-4xl uppercase duration-150 hover:cursor-pointer"
                   href={link.href}
                 >
                   {link.display}
@@ -24,6 +42,6 @@ export default function MenuModal() {
           })}
         </nav>
       </div>
-    </div>
+    </motion.section>
   );
 }
