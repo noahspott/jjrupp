3dbe259c11dee973f620e4a4177d83267aa9cb15
noahspott <noahspottdev@gmail.com>
2025-07-23 12:52:32 -0300

    chore: update events

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,147 +1,101 @@
 import type { EventType } from "@/components/schedule/EventCard.astro";
 
+type CreateEventDateInput = {
+  year: number;
+  month: number; // 1 = January, 12 = December
+  day: number;
+  hour?: number; // 24-hour format, default = 0
+  minute?: number; // default = 0
+};
+
+const createEventDate = ({
+  year,
+  month,
+  day,
+  hour = 0,
+  minute = 0,
+}: CreateEventDateInput): Date => {
+  return new Date(Date.UTC(year, month - 1, day, hour, minute));
+};
+
 export const events: EventType[] = [
   {
-    date: new Date("2025-01-18T20:00:00"),
+    date: createEventDate({
+      year: 2025,
+      month: 8,
+      day: 15,
+    }),
     timezone: "America/New_York",
     venue: {
-      name: "The Grand Theater at Foxwoods Resort Casino",
+      name: "Ugly Mug",
       address: {
-        street: "350 Trolley Line Blvd",
-        city: "Mashantucket",
-        state: "CT",
+        street: "426 Washington Street",
+        city: "Cape May",
+        state: "NJ",
       },
     },
   },
   {
-    date: new Date("2025-02-14T19:30:00"),
-    timezone: "America/Chicago",
-    venue: {
-      name: "Joe's Live Rosemont",
-      address: {
-        street: "5441 Park Pl, Parkway Bank Park Entertainment District",
-        city: "Rosemont",
-        state: "IL",
-      },
-    },
-  },
-  {
-    date: new Date("2025-03-09T20:00:00"),
-    timezone: "America/Los_Angeles",
-    venue: {
-      name: "Golden 1 Center",
-      address: {
-        street: "500 David J Stern Walk",
-        city: "Sacramento",
-        state: "CA",
-      },
-    },
-  },
-  {
-    date: new Date("2025-04-22T18:30:00"),
-    timezone: "America/Denver",
-    venue: {
-      name: "Red Rocks Amphitheatre",
-      address: {
-        street: "18300 W Alameda Pkwy",
-        city: "Morrison",
-        state: "CO",
-      },
-    },
-  },
-  {
-    date: new Date("2025-05-17T19:00:00"),
+    date: createEventDate({
+      year: 2025,
+      month: 8,
+      day: 30,
+    }),
     timezone: "America/New_York",
     venue: {
-      name: "The Fillmore Philadelphia",
+      name: "Paddy's Green",
       address: {
-        street: "29 E Allen St",
-        city: "Philadelphia",
+        street: "3907 Landis Avenue",
+        city: "Sea Isle City",
+        state: "NJ",
+      },
+    },
+  },
+  {
+    date: createEventDate({
+      year: 2025,
+      month: 9,
+      day: 5,
+    }),
+    timezone: "America/New_York",
+    venue: {
+      name: "Ugly Mug",
+      address: {
+        street: "426 Washington Street",
+        city: "Cape May",
+        state: "NJ",
+      },
+    },
+  },
+  {
+    date: createEventDate({
+      year: 2025,
+      month: 10,
+      day: 11,
+    }),
+    timezone: "America/New_York",
+    venue: {
+      name: "Legends Live",
+      address: {
+        street: "2545 North 5th Street Highway",
+        city: "Reading",
         state: "PA",
       },
     },
   },
   {
-    date: new Date("2025-06-12T20:00:00"),
-    timezone: "America/Chicago",
+    date: createEventDate({
+      year: 2025,
+      month: 12,
+      day: 6,
+    }),
+    timezone: "America/New_York",
     venue: {
-      name: "Stage AE Outdoor Summer Concert Series Presented by Coors Light",
+      name: "Legends Live",
       address: {
-        street: "400 North Shore Dr",
-        city: "Pittsburgh",
+        street: "2545 North 5th Street Highway",
+        city: "Reading",
         state: "PA",
-      },
-    },
-  },
-  {
-    date: new Date("2025-07-26T21:00:00"),
-    timezone: "America/Phoenix",
-    venue: {
-      name: "The Van Buren",
-      address: {
-        street: "401 W Van Buren St",
-        city: "Phoenix",
-        state: "AZ",
-      },
-    },
-  },
-  {
-    date: new Date("2025-08-15T19:00:00"),
-    timezone: "America/New_York",
-    venue: {
-      name: "The Bluebird Cafe",
-      address: {
-        street: "4104 Hillsboro Pike",
-        city: "Nashville",
-        state: "TN",
-      },
-    },
-  },
-  {
-    date: new Date("2025-09-05T20:00:00"),
-    timezone: "America/Chicago",
-    venue: {
-      name: "Billy Bob's Texas",
-      address: {
-        street: "2520 Rodeo Plaza",
-        city: "Fort Worth",
-        state: "TX",
-      },
-    },
-  },
-  {
-    date: new Date("2025-10-18T19:30:00"),
-    timezone: "America/New_York",
-    venue: {
-      name: "The Charleston Music Hall",
-      address: {
-        street: "37 John St",
-        city: "Charleston",
-        state: "SC",
-      },
-    },
-  },
-  {
-    date: new Date("2025-11-09T20:00:00"),
-    timezone: "America/Chicago",
-    venue: {
-      name: "Ryman Auditorium",
-      address: {
-        street: "116 5th Ave N",
-        city: "Nashville",
-        state: "TN",
-      },
-    },
-  },
-  {
-    date: new Date("2025-12-21T19:00:00"),
-    timezone: "America/New_York",
-    venue: {
-      name: "Beacon Theatre - Holiday Country Nights Series",
-      address: {
-        street: "2124 Broadway",
-        city: "New York",
-        state: "NY",
       },
     },
   },
