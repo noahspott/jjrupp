3d1062a315b61ee398515525a22deeeb30b273a0
noahspott <noahspottdev@gmail.com>
2025-07-25 11:29:47 -0300

    feat: opengraph

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as Seo } from "./Seo.astro";
 export { default as Favicon } from "./Favicon.astro";
 export { default as FontPreloaders } from "./FontPreloaders.astro";
 export { default as ThemeMeta } from "./ThemeMeta.astro";
+export { default as OpenGraph } from "./OpenGraph.astro";
